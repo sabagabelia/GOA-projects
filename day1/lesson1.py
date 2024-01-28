8e33f7bd969720e47f8addd4c6f500384e7ebd6c
sabagabelia <jujula777111@gmail.com>
2024-01-28 15:06:30 +0400

    first commit

---
+++ day1/lesson1.py
@@ -0,0 +1,8 @@
+name = "saba"
+surname = "gabelia"
+age = 15
+height = 169.10 
+knows_programming = True
+is_ugly = False
+print( ("me var") + " " + name + " " + surname + " " + ("var") +  " " + str(age) + " " + ("wlis") + " " + ("simagle") + " " + str(height) + " " + ("var") + " " + str(knows_programming) + " " + ("wyalburteli")      )
+
