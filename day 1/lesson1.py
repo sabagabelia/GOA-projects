54e50253c4238cd892cb1cf446221c2647bb1370
sabagabelia <jujula777111@gmail.com>
2024-02-09 21:17:17 +0400

    second commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,8 @@
+name = "saba"
+surname = "gabelia"
+age = 15
+height = 169.10 
+knows_swimming = True
+is_ugly = False
+print( ("me var") + " " + name + " " + surname + " " + ("var") +  " " + str(age) + " " + ("wlis") + " " + ("simagle") + " " + str(height) + " " + ("var") + " " + str(knows_swimming) + " " + ("wyalburteli")      )
+
--- day1/lesson1.py
+++
@@ -1,8 +0,0 @@
-name = "saba"
-surname = "gabelia"
-age = 15
-height = 169.10 
-knows_programming = True
-is_ugly = False
-print( ("me var") + " " + name + " " + surname + " " + ("var") +  " " + str(age) + " " + ("wlis") + " " + ("simagle") + " " + str(height) + " " + ("var") + " " + str(knows_programming) + " " + ("wyalburteli")      )
-
